22d34cc7f87dca75162b2513203f55cb5b8f287b
FransiskusAdrianto <157003214+FransiskusAdrianto@users.noreply.github.com>
2026-01-08 16:53:13 +0700

    update produk dan modal dan cart

--- Website Warung Mbak Inn/javascript/script.js
+++ Website Warung Mbak Inn/javascript/script.js
@@ -8,18 +8,25 @@ document.querySelector("#hamburger-menu").onclick = (e) => {
   e.preventDefault();
 };
 
-// toggle class active search form
+// toggle class active search form & shopping cart
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
+const shoppingCart = document.querySelector(".shopping-cart");
 document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active");
   searchBox.focus();
   e.preventDefault();
 };
 
+document.querySelector("#shooping-cart").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 //klik diluar sidebar buat menghilangkan menu
 const hamburger = document.querySelector("#hamburger-menu");
 const searchButton = document.querySelector("#search-button");
+const shoppingCartButton = document.querySelector("#shooping-cart");
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
@@ -27,5 +34,12 @@ document.addEventListener("click", function (e) {
 
   if (!searchForm.contains(e.target) && !searchButton.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+
+  if (
+    !shoppingCart.contains(e.target) &&
+    !shoppingCartButton.contains(e.target)
+  ) {
+    shoppingCart.classList.remove("active");
   }
 });
