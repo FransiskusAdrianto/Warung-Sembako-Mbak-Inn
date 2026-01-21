a763559896dd1efd83733c28e403c0c65d6217e7
FransiskusAdrianto <157003214+FransiskusAdrianto@users.noreply.github.com>
2026-01-21 12:15:10 +0600

    nambah svg dan app js

--- Website Warung Mbak Inn/javascript/app.js
+++ Website Warung Mbak Inn/javascript/app.js
@@ -14,11 +14,33 @@ document.addEventListener("alpine:init", () => {
         price: 26000,
       },
       {
-        id: 2,
+        id: 3,
         name: "Gas",
         img: "3.jpg",
         price: 22000,
       },
     ],
   }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      this.items.push(newItem);
+      this.quantity++;
+      this.total += newItem.price;
+      console.log(this.total);
+    },
+  });
 });
+
+// konversi rupiah
+
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    // minimumFractionDigits: 0,
+  }).format(number);
+};
